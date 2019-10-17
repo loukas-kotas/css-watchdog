4102f9281078e698d7cad0b19c8629d37f9df21d
loukas.kotas <loukas.kotas@gmail.com>
2019-10-18 00:46:52 +0300

    gh-#12 config index.js  for cli

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,16 +1,47 @@
 const program = require('commander');
-const { get_attribute_of_element } = require('./commands');
+const { 
+    get_attribute_of_element,
+    screenshot_whole_page
+} = require('./commands');
+const { run_through_configuration } = require("./configuration");
+const fs = require("fs");
 
+var contents = fs.readFileSync("./package.json");
+var packageJson = JSON.parse(contents);
+
+program
+    .option('-d, --debug', 'output extra debugging')
+    .option('-a, --amend', 'output extra debugging')
+
+
+program.on('--help', function(){
+    console.log('')
+    console.log('Examples:');
+    console.log('  $ attribute https://loukaskotas.com experience font-size');
+});
+    
 
 program
     .version('0.0.1')
-    .description('Command line Weather Application')
+    .description(`CSS-Watchdog CLI - v${packageJson.version}`)
 
 program
     .command("attribute <source> <elementId> <attribute>")
     .alias('n')
     .description('see the current weather in the specified city')
     .action((source, elementId, attribute) => get_attribute_of_element( source, elementId, attribute));
+
+program
+    .command("screenshot-page <source>")
+    .alias('n')
+    .description('see the current weather in the specified city')
+    .action((source) => screenshot_whole_page(source));
+
+    program
+    .command("config")
+    .alias('n')
+    .description('see the current weather in the specified city')
+    .action(() => run_through_configuration());
 
 
 program.parse(process.argv)
