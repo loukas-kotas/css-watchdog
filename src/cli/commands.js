a7faa464ae987c94b68b2f8d2065ef778e8543fb
loukas.kotas <loukas.kotas@gmail.com>
2019-10-18 00:40:45 +0300

    gh-#12 update cli commands

--- src/cli/commands.js
+++ src/cli/commands.js
@@ -13,7 +13,14 @@ const get_attribute_of_element = async (source, elementId, attribute) => {
     })
 };
 
+const screenshot_whole_page = async (source) => {
+    const result = await facade().screenshot_whole_page(source);
+    process.exit(result);
+}
+
 
 module.exports = {
-    get_attribute_of_element:get_attribute_of_element
+    get_attribute_of_element:get_attribute_of_element,
+    screenshot_whole_page: screenshot_whole_page
+
 }
