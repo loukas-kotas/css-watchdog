63a7979ccaf016993f5f8b38b564b522f60c5a99
loukas.kotas <loukas.kotas@gmail.com>
2019-10-18 23:57:16 +0300

    gh-#12 remove dead code

--- src/cli/configuration.js
+++ src/cli/configuration.js
@@ -5,12 +5,7 @@ const facade = require('../logic/facade');
 const errorHandler = require('../logic/error-handler.controller');
 const fs = require('fs');
 
-// var contents = fs.readFileSync("./cli-config.json");
-// var conf = JSON.parse(contents);
-
-
 const run_through_configuration = async () => {
-    // handleCases(conf);
 }
 
 module.exports = {
