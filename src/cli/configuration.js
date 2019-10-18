170c68bbb71f22f1107f9c6e1e8359d5b63e66ee
loukas.kotas <loukas.kotas@gmail.com>
2019-10-18 22:43:24 +0300

    gh-#12 add configuration.js

---
+++ src/cli/configuration.js
@@ -0,0 +1,18 @@
+
+
+
+const facade = require('../logic/facade');
+const errorHandler = require('../logic/error-handler.controller');
+const fs = require('fs');
+
+// var contents = fs.readFileSync("./cli-config.json");
+// var conf = JSON.parse(contents);
+
+
+const run_through_configuration = async () => {
+    // handleCases(conf);
+}
+
+module.exports = {
+    run_through_configuration: run_through_configuration
+}
