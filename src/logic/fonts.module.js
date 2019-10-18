cc70ff6971e4190340d743e83ced40bb1be0c2c2
loukas.kotas <loukas.kotas@gmail.com>
2019-10-19 01:17:01 +0300

    gh-#22 add get_attribute_of_element method

--- src/logic/fonts.module.js
+++ src/logic/fonts.module.js
@@ -19,12 +19,27 @@ const export_module = (function() {
         });
     }
 
+    async function get_attribute_of_element(source, elementId, attribute) {
+        
+        const browser = await puppeteer.launch();
+        const page    = await browser.newPage();
+        await page.goto(source);
+
+        return await page.evaluate((elementId, attribute) => {
+            const element = document.getElementById(elementId);
+            
+            return window.getComputedStyle(element).getPropertyValue(attribute);
+
+        }, elementId, attribute);
+    }
+
 
     // FACADE
     return  {
         get_fonts: async function(source) {
             return get_all_fonts_from_website(source)
-        }
+        },
+        get_attribute_of_element: get_attribute_of_element
     }
 });
 
