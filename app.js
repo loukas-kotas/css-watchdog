ee665cd4ec42c5e0a47e897a4dbfeee5aea9a9a6
loukas.kotas <loukas.kotas@gmail.com>
2019-10-27 00:43:55 +0300

    use environment variables

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express')
 const bodyParser = require('body-parser');
-var app = express()
+const dotenv = require('dotenv');
+var app = express();
+dotenv.config();
 
 app.use("/static", express.static(__dirname + "/src"));
 app.use(bodyParser.json())
 app.use(require('./src/routes'));
 
 const port = 3000;
-app.listen(port, () => { console.log(`I am running at ${port}!`)});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {    (`I am running at ${port}!`)});
